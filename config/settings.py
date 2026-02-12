dba1550e36e7d28954421eb72e026159801e8c22
Julio Scarceli <103069514+julioscarceli@users.noreply.github.com>
2026-02-12 16:28:23 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,12 @@
-# config/settings.py (VERSÃO FINAL DE DEPLOY COERENTE)
+# config/settings.py
 
-# --- URLs de Acesso ao SISTEMA (Para Login/Web Scraping) ---
+# --- URLs de Acesso ao SISTEMA ---
 LOGIN_URL_MG = "http://186.194.50.155/azcall/pages/login.php"
 LOGIN_URL_SP = "https://186.194.50.149/azcall/pages/login.php"
+
+# --- URLs de FILAS (ADICIONAR ESTAS LINHAS) ---
+URL_FILAS_SP = "https://186.194.50.149/azcall/pages/filas.php"
+URL_FILAS_MG = "http://186.194.50.155/azcall/pages/filas.php"
 
 # --- URLs Base da API ---
 BASE_URL_MG = "http://186.194.50.155"
@@ -13,12 +17,7 @@ FILA_NOME_MG = "DISCADOR_MG"
 FILA_NOME_SP = "DISCADOR_SP"
 SAIDAS_VALOR = "130" 
 
-# --- CONTROLE DE SEGURANÇA ---
-API_TOKEN_NAME = "API_TOKEN" 
-
-# --- CAMINHOS DE MAILING LOCAIS ---
-LOCAL_MAILING_BASE_DIR = r"D:\Ferramentas\5. Verificação Final\MAILING DISCADOR"
-
-# IDs Oficiais das Campanhas no Discador
+# IDs Oficiais das Campanhas
 ID_CAMPANHA_MG = "20"
 ID_CAMPANHA_SP = "10"
+
